45b6459aa66f562ba4eb83f506c31b8d3294765c
pomoke <pomoke@typeof.pw>
2019-02-13 05:03:52 +0800

    Added VGA cursor

--- system.c
+++ system.c
@@ -31,8 +31,8 @@ asmlink void spin_up(void)
 	fb[2]='o';
 	fb[4]='k';
 	fb[6]='e'; */
-	print("MOKE by pomoke,alpha version",10,0);
-	print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",15,1);
+	print("MOKE by pomoke,alpha version\n",10,0);
+	print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n",15,1);
 	print("!",0,2);
 	for (;;) ;
 }
--- vga.c
+++ vga.c
@@ -1,13 +1,28 @@
 #include "header/type.h"
 #include "header/link.h"
+#include "header/io.h"
 
+#define VGA_MMIO_ADDR 0xb8000
+#define VGA_WIDTH 80
+#define VGA_CMD_PORT 0x3d4
+#define VGA_DATA_PORT 0x3d5
+#define VGA_IO_HIGH 0xe
+#define VGA_IO_LOW 0xf
 int pos_x=0,pos_y=0;
+
+void vga_cursor(u32 x,u32 y)
+{
+	outb(VGA_CMD_PORT,VGA_IO_HIGH);
+	outb(VGA_DATA_PORT,(((y*VGA_WIDTH+x)>>8)&0xff));
+	outb(VGA_CMD_PORT,VGA_IO_LOW);
+	outb(VGA_DATA_PORT,(y*VGA_WIDTH+x)&0xff);
+}
 
 void vga_write(u32 x,u32 y,char c,u8 color)
 {
-	u8 *fb=(u8 *)0xb8000;
-	fb[2*(x+y*80)]=c;
-	fb[2*(x+y*80)+1]=color;
+	u8 *fb=(u8 *)VGA_MMIO_ADDR;
+	fb[2*(x+y*VGA_WIDTH)]=c;
+	fb[2*(x+y*VGA_WIDTH)+1]=color;
 	return;
 }
 
@@ -16,16 +31,23 @@ void print(char * str,u8 fg,u8 bg)
 	char *p=str;
 	while (*p)
 	{
-		vga_write(pos_x,pos_y,*p,((bg&0x0f)<<4|(fg&0x0f)));
-		pos_x++;
-		if (pos_x>=80)
+		if (*p=='\n')
+		{
+			pos_y++;
+			pos_x=0;
+		}
+		else 
+		{
+			vga_write(pos_x,pos_y,*p,((bg&0x0f)<<4|(fg&0x0f)));
+			pos_x++;
+		}
+		if (pos_x>=VGA_WIDTH)
 		{
 			pos_x=0;
 			pos_y++;
 		}
 		p++;
 	}
-	pos_x=0;
-	pos_y++;
+	vga_cursor(pos_x-1,pos_y);
 	return;
 }
