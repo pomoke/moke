6546734811a58f9670c6e6c5e6e09e641981597c
pomoke <pomoke@typeof.pw>
2019-02-13 05:03:01 +0800

    Added VGA cursor

---
+++ README.md
@@ -0,0 +1,3 @@
+#MOKE
+
+A toy os developed by pomoke,with the help of littleosbook and osdev.
--- header/io.h
+++ header/io.h
@@ -1,9 +1,52 @@
 #include "link.h"
 
-void outb(,)
+static inline void outb(u16 port,u8 val)
 {
 	asm volatile(
-		""
-		""
-		""
-		)
+		"outb %0,%1"::
+		"a"(val),"Nd"(port)
+		);
+	return;
+}
+
+static inline void outw(u16 port,u16 val)
+{
+	asm volatile(
+		"outw %0,%1"::
+		"a"(val),"Nd"(port)
+		);
+	return;
+}
+static inline void outl(u16 port,u32 val)
+{
+	asm volatile(
+		"outl %0,%1"::
+		"a"(val),"Nd"(port)
+		);
+	return;
+}
+
+static inline u8 inb(u16 port)
+{
+	u8 ret;
+	asm volatile("inb %1,%0"
+			:"=a"(ret)
+			:"Nd"(port));
+	return ret;
+}
+static inline u16 inw(u16 port)
+{
+	u16 ret;
+	asm volatile("inw %1,%0"
+			:"=a"(ret)
+			:"Nd"(port));
+	return ret;
+}
+static inline u32 inl(u16 port)
+{
+	u32 ret;
+	asm volatile("inb %1,%0"
+			:"=a"(ret)
+			:"Nd"(port));
+	return ret;
+}
