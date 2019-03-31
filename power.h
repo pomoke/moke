70a8892532a43860afb7bdafe15ad925076111c4
pomoke <pomoke@typeof.pw>
2019-03-31 10:55:17 +0800

    Fix previous commit

---
+++ power.h
@@ -0,0 +1,23 @@
+#include "header/io.h"
+#include "header/type.h"
+void reboot()
+{
+	//Reboot by 8042 kbd controller
+	u8 good=0x02;
+	volatile u32 time=0;
+	kprint_vga("Rebooting...\n",15,0);
+	while (time<100000000)
+		time++;
+	while (good & 0x02)
+		good=inb(0x64);
+	outb(0x64,0xFE);
+}
+
+void poweroff_qemu()
+{
+	volatile u32 time=0;
+	kprint_vga("Powering off...\n",15,0);
+	while (time<100000000)
+		time++;
+	outw(0x604,0x2000);
+}
